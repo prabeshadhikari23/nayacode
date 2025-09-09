a0ffb851becd7ef3ff2f9d43d38ed84739d5bad0
prabeshadhikari23 <prabesh.adhikari23@gmail.com>
2025-09-09 09:14:45 +0545

    Fix SEO import in Partners.tsx

--- src/pages/Partners.tsx
+++ src/pages/Partners.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SEO } from '../components/SEO';
+import SEO from '../components/SEO';
 import { useCMS } from '../components/cms/CMSProvider';
 import { EditableText } from '../components/cms/EditableText';
 
