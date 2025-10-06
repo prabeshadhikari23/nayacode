40c3b26f99009b2a323df57b62e6b93ee68829a3
prabeshadhikari23 <prabesh.adhikari23@gmail.com>
2025-10-06 09:57:41 +0545

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -292,7 +292,7 @@ const Contact = () => {
                     width="100%" 
                     height="100%" 
                     style={{border:0}} 
-                    allowFullScreen 
+                    allowFullScreen={true}
                     loading="lazy" 
                     referrerPolicy="no-referrer-when-downgrade"
                     title="Naya Code Location"
